82353effeaa40bf1de01e6130fe7b7bca1fd4e07
Iliyas Bolatbek <122792624+iliyasbt0569@users.noreply.github.com>
2025-02-23 04:32:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,54 @@ if (targetElement) {
 
 function injectSyntaxHighlighterStyles() {
     const styleContent = `
+    :root[theme='dark'] {
+	--body-bg-color: #191B20;
+	--container-bg-color: #16213E;
+	--menc-bg-color:#191B20;
+	--body-text-color: #F9F9F9;
+	--mentA-text-color: #F9F9F9;
+	--fa-border-color:#F9F9F9;
+	--fa-hover-color:#F9F9F9;
+	--a-text-color:#E94560;
+	--h-text-color:#F9F9F9;
+	--menu-bg-color: #0F3460;
+	--menu-second-bg-color: #0F3460;
+	--menu-text-color:#F9F9F9;
+	--menu-text-shadow-width: 0px;
+	--sprite-img: url(https://metanit.com/img/sprites4.png);
+	--codef-bgcolor:#1A1A2E;
+	--code-bgcolor:#16213E;
+	--code-keyword:#E94560;
+	--code-plain:#F9F9F9;
+	--code-comment:#0F3460;
+	--code-string:#E94560;
+	--tree-selcolor:#E94560;
+	--tree-selbgcolor:#16213E;
+	--fa-adjust:"\f185";
+	--bottom-color:#F9F9F9;
+	--socbtn-color:#F9F9F9;
+	--logo-color:#F9F9F9;
+	--logo-hover-color:#E94560;
+	--legend-text-color: #F9F9F9;
+	--dop-txt-color:#F9F9F9;
+	--dop-hover-txt-color:#E94560;
+	--console-bgcolor:#0F3460;
+	--console-textcolor:#F9F9F9;
+	--contpage-color:#F9F9F9;
+	--content-a-seltxtcolor:#F9F9F9;
+	--tabhead-bgcolor:#16213E;
+	--tab-bgcolor:#0F3460;
+}
+
+code {
+  font-family: monospace;
+  font-weight: bold;
+  border: 1px #256fb0 solid;
+  padding: 5px;
+  margin: 6px;
+  border-radius: 4px;
+}
+
        .syntaxhighlighter {
     width: 99% !important;
     margin: 1em 0 1em -4px !important;
